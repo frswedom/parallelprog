c7fd7d601422595590156defb690f13179177909
qqqwm <qqqwm@yandex.ua>
2019-10-20 23:56:13 +0300

    Added zrhqr algo to mcint2.c
    wwc would be 10% less

--- mcint2.c
+++ mcint2.c
@@ -9,6 +9,35 @@ double f(double a)
     return a*a;
 }
 
+void zrhqr(float a[], int m, float rtr[], float rti[])
+{
+    void balanc(float **a, int n);
+    void hqr(float **a, int n, float wr[], float wi[]);
+    int j,k;
+    float **hess,xr,xi;
+
+    hess=matrix(1,MAXM,1,MAXM);
+    if (m > MAXM || a[m] == 0.0) nrerror("bad args in zrhqr");
+    for (k=1;k<=m;k++) {
+        hess[1][k] = -a[m-k]/a[m];
+        for (j=2;j<=m;j++) hess[j][k]=0.0;
+        if (k != m) hess[k+1][k]=1.0;
+    }
+    balanc(hess,m);
+    hqr(hess,m,rtr,rti);
+    for (j=2;j<=m;j++) {
+        xr=rtr[j];
+        xi=rti[j];
+        for (k=j-1;k>=1;k--) {
+            if (rtr[k] <= xr) break;
+            rtr[k+1]=rtr[k];
+            rti[k+1]=rti[k];
+        }
+        rtr[k+1]=xr;
+        rti[k+1]=xi;
+    }
+    free_matrix(hess,1,MAXM,1,MAXM);
+}
 int main(int argc, char const *argv[])
 {
     int id, procnum;
@@ -44,6 +73,7 @@ int main(int argc, char const *argv[])
         MPI_Reduce(&mc_int_val, &mc_int[j], 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     }
     
+    zrhqr(mcint, 8, reff, quality_res);
 
     //-------------------------------------------------------------------------
     // simple rectangle. only outer loop will be paralleled
